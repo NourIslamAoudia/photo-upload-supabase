1ff6aae0627adc8a766e5bd4cb59cdfe38fd8eed
NorIslam04 <islamaoudi18@gmail.com>
2025-02-11 20:48:05 +0100

    edit

--- app.js
+++ app.js
@@ -10,16 +10,12 @@ const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 // Configuration de Multer (utilisation de memoryStorage)
-const upload = multer({
-  storage: multer.memoryStorage(),
-  limits: { fileSize: 10 * 1024 * 1024 }, // 10 Mo
-});
+const upload = multer({ storage: multer.memoryStorage() });
 
 // Initialisation d'Express
 const app = express();
-const port = process.env.PORT || 3000;
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
+const port = 3000;
+app.use(express.json());
 app.use(cors());
 
 // Route pour uploader une photo
